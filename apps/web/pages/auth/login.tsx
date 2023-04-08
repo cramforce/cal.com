3f65044f448a1b2c8a6d7fad07024e4e41592099
Malte Ubl <malte.ubl@gmail.com>
2023-04-08 12:03:57 -0700

    prisma

--- apps/web/pages/auth/login.tsx
+++ apps/web/pages/auth/login.tsx
@@ -1,13 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export default function Login() {
-  return <h1>hello world</h1>;
+export default function Login(props: any) {
+  return <pre>{JSON.stringify(props, null, "  ")}</pre>;
 }
+
+let hot = false;
 
 // TODO: Once we understand how to retrieve prop types automatically from getServerSideProps, remove this temporary variable
 export const getServerSideProps = async function getServerSideProps(): Promise<any> {
+  const wasHot = hot;
+  hot = true;
+  const before = Date.now();
+  await import("@calcom/prisma");
   return {
-    props: {},
+    props: {
+      after: Date.now() - before,
+      hot: wasHot,
+    },
   };
 };
 
